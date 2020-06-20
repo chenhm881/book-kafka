bead38a6e209e395109d2c6c0f6938168fbaa869
huiming chen <v-huchen@expedia.com>
2020-06-20 23:44:19 +0800

    init commit

---
+++ src/main/java/com/heiio/bookkafka/BookKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.heiio.bookkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookKafkaApplication.class, args);
+    }
+
+}
